a95c3bd62267e430996c43c812d6a8f09f1656e3
Aditya Viswanatham <a.r.viswanatham@gmail.com>
2021-12-06 00:20:38 -0600

    Formatted code

--- src/com/avi/server/ServerWorker.java
+++ src/com/avi/server/ServerWorker.java
@@ -8,42 +8,43 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 /**
- *	Thread class to communicate with client
- *	New worker is spawned for every incoming connection
+ * Thread class to communicate with client New worker is spawned for every
+ * incoming connection
  */
 public class ServerWorker extends Thread {
 
 	private final Socket clientSocket;
-	
+
 	public ServerWorker(Socket clientSocket) {
 		this.clientSocket = clientSocket;
 	}
-	
+
 	@Override
 	public void run() {
 		try {
 			handleClientSocket();
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void handleClientSocket() throws IOException, InterruptedException {
+
 		InputStream inputStream = clientSocket.getInputStream();
-    	BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-    	OutputStream outputStream = clientSocket.getOutputStream();
-    	
-       	String line;
-    	while ((line=reader.readLine()) != null) {
-    		if (line.equalsIgnoreCase("quit"))
-    			clientSocket.close();
-    		else {
-    			String msg = "You typed: " + line + "\n";
-    			outputStream.write(msg.getBytes());
-    		}
-    	}
-    }
+		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+		OutputStream outputStream = clientSocket.getOutputStream();
+
+		String line;
+		while ((line = reader.readLine()) != null) {
+			if (line.equalsIgnoreCase("quit"))
+				clientSocket.close();
+			else {
+				String msg = "You typed: " + line + "\n";
+				outputStream.write(msg.getBytes());
+			}
+		}
+	}
 }

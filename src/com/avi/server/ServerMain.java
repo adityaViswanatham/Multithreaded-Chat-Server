e5db86d948ec92060a899cae6b047c6b8a6f7d46
Aditya Viswanatham <a.r.viswanatham@gmail.com>
2021-12-06 23:49:23 -0600

    Added support for user presence

--- src/com/avi/server/ServerMain.java
+++ src/com/avi/server/ServerMain.java
@@ -1,30 +1,11 @@
 package com.avi.server;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 public class ServerMain {
 
 	final static int PORT = 8181;
 
 	public static void main(String[] args) {
-
-		try {
-
-			@SuppressWarnings("resource")
-			ServerSocket serverSocket = new ServerSocket(PORT);
-
-			while (true) {
-				System.out.println("Ready to accept client connection...");
-				Socket clientSocket = serverSocket.accept();
-				System.out.println("Connection accepted from " + clientSocket);
-				ServerWorker worker = new ServerWorker(clientSocket);
-				worker.start();
-			}
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		Server server = new Server(PORT);
+		server.start();
 	}
 }

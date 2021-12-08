25c2fff48d28cafe6f41cdc885be33d17757442f
Aditya Viswanatham <a.r.viswanatham@gmail.com>
2021-12-08 01:06:14 -0600

    Added direct messages and broadcast messages

--- src/com/avi/server/ServerWorker.java
+++ src/com/avi/server/ServerWorker.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.HashSet;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
@@ -17,16 +18,15 @@ import org.apache.commons.lang3.StringUtils;
 public class ServerWorker extends Thread {
 
 	private final Socket clientSocket;
-
 	private Server server;
-
 	private String userName = null;
-
 	private OutputStream outputStream;
+	private HashSet<String> channels;
 
 	public ServerWorker(Server server, Socket clientSocket) {
 		this.server = server;
 		this.clientSocket = clientSocket;
+		channels = new HashSet<>();
 	}
 
 	@Override
@@ -54,32 +54,122 @@ public class ServerWorker extends Thread {
 			String[] tokens = StringUtils.split(line);
 			if (tokens != null && tokens.length > 0) {
 				String cmd = tokens[0];
-				if (cmd.equalsIgnoreCase("quit") || cmd.equalsIgnoreCase("logout"))
+				if (cmd.equalsIgnoreCase("quit") || cmd.equalsIgnoreCase("logout")) {
 					handleLogout();
-				else if (cmd.equalsIgnoreCase("login"))
+					break;
+				} else if (cmd.equalsIgnoreCase("login"))
 					handleLogin(outputStream, tokens);
-				else {
+				else if (cmd.equalsIgnoreCase("msg")) {
+					String[] msgTokens = StringUtils.split(line, null, 3);
+					handleMessage(msgTokens);
+				} else if (cmd.equalsIgnoreCase("join")) {
+					String[] joinTokens = StringUtils.split(line, null, 2);
+					handleJoin(joinTokens);
+				} else if (cmd.equalsIgnoreCase("leave")) {
+					String[] leaveTokens = StringUtils.split(line, null, 2);
+					handleLeave(leaveTokens);
+				} else {
 					String msg = "unknown " + cmd + "\n";
-					outputStream.write(msg.getBytes());
+					send(msg);
 				}
 			}
 		}
 	}
 
-	private void handleLogout() throws IOException {
+	// command format: "leave #topic1 ..."
+	private void handleLeave(String[] tokens) throws IOException {
 		List<ServerWorker> workerList = server.getWorkerList();
-		ServerWorker toRemove = null;
-		
+		if (tokens.length > 1) {
+			String[] channelTokens = StringUtils.split(tokens[1]);
+			for (int i = 0; i < channelTokens.length; i++) {
+				String channel = channelTokens[i];
+				this.channels.remove(channel);
+				String msg = "Successfully left " + channel + "\n";
+				send(msg);
+				
+				// notify other people in the channel
+				for (ServerWorker worker: workerList) {
+					if (!this.userName.equals(worker.getUserName()) && worker.isChannelMember(channel)) {
+						String broadcastMsg = this.userName + " has left " + channel + "\n";
+						worker.send(broadcastMsg);
+					}
+				}
+			}
+		}
+	}
+
+	public boolean isChannelMember(String topic) {
+		return this.channels.contains(topic);
+	}
+
+	// command format: "join #topic1 ..."
+	private void handleJoin(String[] tokens) throws IOException {
+		List<ServerWorker> workerList = server.getWorkerList();
+		if (tokens.length > 1) {
+			String[] channelTokens = StringUtils.split(tokens[1]);
+			for (int i = 0; i < channelTokens.length; i++) {
+				String channel = channelTokens[i];
+				this.channels.add(channel);
+				String msg = "Successfully joined " + channel + "\n";
+				send(msg);
+				
+				// notify other people in the channel
+				for (ServerWorker worker: workerList) {
+					if (!this.userName.equals(worker.getUserName()) && worker.isChannelMember(channel)) {
+						String broadcastMsg = this.userName + " has joined " + channel + "\n";
+						worker.send(broadcastMsg);
+					}
+				}
+			}
+		}
+	}
+
+	// command format: "msg recipient(#topic) msgBody"
+	// command format: "msg recipient(userName) msgBody"
+	private void handleMessage(String[] tokens) throws IOException {
+		if (tokens.length == 3) {
+			List<ServerWorker> workerList = server.getWorkerList();
+			String recipient = tokens[1];
+			String msgBody = tokens[2];
+
+			boolean isChannel = recipient.charAt(0) == '#';
+			for (ServerWorker worker : workerList) {
+				if (isChannel) {
+					if (isChannelMember(recipient)) {
+						if (worker.isChannelMember(recipient)) {
+							String msg = "(" + recipient + ") " + this.userName + ": " + msgBody + "\n";
+							worker.send(msg);
+						}
+					} else {
+						String msg = "You are not a channel member\n";
+						send(msg);
+						break;
+					}
+				} else {
+					if (recipient.equals(worker.getUserName())) {
+						String msg = this.userName + ": " + msgBody + "\n";
+						worker.send(msg);
+					}
+				}
+			}
+		} else {
+			String msg = "Incorrect command for messaging\n";
+			send(msg);
+		}
+	}
+
+	private void handleLogout() throws IOException {
+		this.server.removeWorker(this);
+		List<ServerWorker> workerList = server.getWorkerList();
+		System.out.println(userName + " has logged out");
+
 		// notify all users about current user logout
 		String broadcastMsg = userName + " has logged out\n";
 		for (ServerWorker worker : workerList) {
 			if (!worker.getUserName().equals(this.userName))
 				worker.send(broadcastMsg);
-			else
-				toRemove = worker;
 		}
-		clientSocket.close();
-		workerList.remove(toRemove);
+		this.clientSocket.close();
 	}
 
 	public String getUserName() {
@@ -127,6 +217,6 @@ public class ServerWorker extends Thread {
 
 	private void send(String msg) throws IOException {
 		if (this.userName != null)
-			outputStream.write(msg.getBytes());
+			this.outputStream.write(msg.getBytes());
 	}
 }
